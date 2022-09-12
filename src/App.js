4042bfd4b2a563efeb05261e5d21b506457f7697
Deniz Gökbaş <dng.develops@gmail.com>
2022-09-13 02:30:24 +0300

    App class component turned into functional component and implemented "useEffect" and "useState" hooks

--- src/App.js
+++ src/App.js
@@ -1,58 +1,96 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import CardList from "./components/card-list/card-list-component";
 import SearchBox from "./components/search-box/search-box-component";
 import "./App.css";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      users: [],
-      searchField: "",
-    };
-  }
+const App = () => {
+  const [searchField, setSearchField] = useState("");
+  const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState(users);
+  console.log("render");
 
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) =>
-        this.setState(() => {
-          return { users: users };
-        })
-      );
-  }
+      .then((users) => setUsers(users));
+  }, []);
 
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase();
-    this.setState(() => {
-      return {
-        searchField,
-      };
-    });
-  };
-
-  render() {
-    //Destructuring
-    const { users, searchField } = this.state;
-    const { onSearchChange } = this;
-
-    const filteredUsers = users.filter((user) => {
+  useEffect(() => {
+    const newfilteredUsers = users.filter((user) => {
       return user.name.toLocaleLowerCase().includes(searchField);
     });
+    setFilteredUsers(newfilteredUsers);
+  }, [users, searchField]);
 
-    return (
-      <div className="App">
-        <h1 className="app-title">User Search</h1>
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
 
-        <SearchBox
-          onChangeHandler={onSearchChange}
-          placeholder="Search Users"
-          className="users-search-box"
-        />
-        <CardList users={filteredUsers} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <h1 className="app-title">User Search</h1>
+
+      <SearchBox
+        onChangeHandler={onSearchChange}
+        placeholder="Search Users"
+        className="users-search-box"
+      />
+      <CardList users={filteredUsers} />
+    </div>
+  );
+};
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       users: [],
+//       searchField: "",
+//     };
+//   }
+
+//   componentDidMount() {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((response) => response.json())
+//       .then((users) =>
+//         this.setState(() => {
+//           return { users: users };
+//         })
+//       );
+//   }
+
+//   onSearchChange = (event) => {
+//     const searchField = event.target.value.toLocaleLowerCase();
+//     this.setState(() => {
+//       return {
+//         searchField,
+//       };
+//     });
+//   };
+
+//   render() {
+//     //Destructuring
+//     const { users, searchField } = this.state;
+//     const { onSearchChange } = this;
+
+//     const filteredUsers = users.filter((user) => {
+//       return user.name.toLocaleLowerCase().includes(searchField);
+//     });
+
+//     return (
+//       <div className="App">
+//         <h1 className="app-title">User Search</h1>
+
+//         <SearchBox
+//           onChangeHandler={onSearchChange}
+//           placeholder="Search Users"
+//           className="users-search-box"
+//         />
+//         <CardList users={filteredUsers} />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
